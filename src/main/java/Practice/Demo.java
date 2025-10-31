5f3ba043c2e27bc0404478d371f2457d8db54206
rahuldurk <165780089+rahuldurk@users.noreply.github.com>
2025-10-31 17:17:46 +0530

    Update Demo.java
    after hello

--- src/main/java/Practice/Demo.java
+++ src/main/java/Practice/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello gajanan");
 		System.out.println("Hello I got project repo");
+		System.out.println("Software testing envoirnment is created");
 		System.out.println("Hello I made changes in this file...")
 		System.out.println("Hello I made changes in this file...2222")
 
