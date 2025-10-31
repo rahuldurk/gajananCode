07a12711d095c279dff72f764011dc92b2a37572
gajananshiv <gajashiv1010@gmail.com>
2025-10-31 16:26:51 +0530

    modified demo.java

--- src/main/java/Practice/Demo.java
+++ src/main/java/Practice/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello gajanan");
-		System.out.println("Hello I got project repo")
+		System.out.println("Hello I got project repo");
+		System.out.println("Hello I made changes in this file...")
 
 	}
 
