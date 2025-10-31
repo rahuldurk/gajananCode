340173b03d376b27a3b19cc9f011d5bad35a0c60
gajananshiv <gajashiv1010@gmail.com>
2025-10-31 18:25:25 +0530

    Changed5

--- src/main/java/Practice/Demo.java
+++ src/main/java/Practice/Demo.java
@@ -6,9 +6,9 @@ public class Demo {
 		System.out.println("Hello gajanan");
 		System.out.println("Hello I got project repo");
 		System.out.println("Software testing envoirnment is created1");
-		System.out.println("Hello I made changes in this file...")
-		System.out.println("Hello I made changes in this file...2222")
-		System.out.println("Hello I made changes in this file...33333")
+		System.out.println("Hello I made changes in this file...");
+		System.out.println("Hello I made changes in this file...2222");
+		System.out.println("Hello I made changes in this file...33333");
 
 
 	}
