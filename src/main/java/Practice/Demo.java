739bdd5878b1bb7c384e29cb809623ec1b1c164c
Rahul Durke <rkdurke09112gmail.com>
2025-10-31 16:57:43 +0530

    envoirnment is created

--- src/main/java/Practice/Demo.java
+++ src/main/java/Practice/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello gajanan");
-		System.out.println("Hello I got project repo")
+		System.out.println("Hello I got project repo");
+		System.out.println("Software testing envoirnment is created");
 
 	}
 
